15ffd1c96a2e850f9578e73017ac640e6ddd6bba
draxtheweabo <draxtheweabo02@gmail.com>
2025-07-23 15:23:09 +0800

    Footer Fix

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,25 +4,6 @@ export const headerData = {
   links: [
     {
       text: 'Modern',
-      // links: [
-      //   {
-      //     text: 'SaaS',
-      //     href: getPermalink('/homes/saas'),
-      //   },
-      //   {
-      //     text: 'Startup',
-      //     href: getPermalink('/homes/startup'),
-      //   },
-      //   {
-      //     text: 'Mobile App',
-      //     href: getPermalink('/homes/mobile-app'),
-      //   },
-      //   {
-      //     text: 'Personal',
-      //     href: getPermalink('/homes/personal'),
-      //   },
-      // ],
-
       href: '/#modernD20',
     },
     {
@@ -41,62 +22,58 @@ export const headerData = {
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Modern d20 SRD',
-      links: [
-        { text: 'Basics', href: 'modern-d20-srd-basics-index' },
-        { text: 'Classes', href: 'modern-d20-srd-classes-index' },
-        { text: 'Combat', href: 'modern-d20-srd-combat-index' },
-        { text: 'Condition Summary', href: 'modern-d20-srd-conditions-index' },
-        { text: 'Creature Types', href: 'modern-d20-srd-creature-types-index' },
-        { text: 'Creatures', href: 'modern-d20-srd-creatures-index' },
-        { text: 'Environment & Hazards', href: 'modern-d20-srd-environment-hazards-index' },
-        { text: 'Equipment', href: 'modern-d20-srd-equipment-index' },
-        { text: 'Feats', href: 'modern-d20-srd-feats-index' },
-      ],
-    },
-    {
-      title: 'Future d20 SRD',
-      links: [
-        { text: 'Classes', href: 'future-d20-srd-classes-index' },
-        { text: 'Cybernetics', href: 'future-d20-srd-cybernetics-index' },
-        { text: 'Environments', href: 'future-d20-srd-environments-index' },
-        { text: 'Equipment', href: 'future-d20-srd-equipment-index' },
-        { text: 'Expanded Skills', href: 'future-d20-srd-expanded-skills-index' },
-        { text: 'Feats', href: 'future-d20-srd-feats-index' },
-        { text: 'Mecha', href: 'future-d20-srd-mecha-index' },
-        { text: 'Mutations', href: 'future-d20-srd-mutations-index' },
-        { text: 'Progress Levels', href: 'future-d20-srd-progress-levels-index' },
-      ],
-    },
-    {
-      title: 'Arcana d20 SRD',
-      links: [
-        { text: 'Advanced Classes', href: 'arcana-d20-srd-advanced-classes-index' },
-        { text: 'Creatures', href: 'arcana-d20-srd-creatures-index' },
-        { text: 'Equipment', href: 'arcana-d20-srd-equipment-index' },
-        { text: 'Feats', href: 'arcana-d20-srd-feats-index' },
-        { text: 'FX Items', href: 'arcana-d20-srd-fx-items-index' },
-        { text: 'Incantations', href: 'arcana-d20-srd-incantations-index' },
-        { text: 'Occupations', href: 'arcana-d20-srd-occupations-index' },
-        { text: 'Organizations', href: 'arcana-d20-srd-organizations-index' },
-        { text: 'Prestige Classes', href: 'arcana-d20-srd-prestige-classes-index' },
-      ],
-    },
-    {
-      title: 'd20 Menaces',
-      links: [
-        { text: 'Buying Services', href: 'menaces-d20-buying-services-index' },
-        { text: 'Feats', href: 'menaces-d20-feats-index' },
-        { text: 'Menaces', href: 'menaces-d20-menaces-index' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-  ],
-  socialLinks: [
-  ],
+links: [
+  {
+    title: 'Modern d20 SRD',
+    links: [
+      { text: 'Basics', href: '/modern.d20.srd/basics' },
+      { text: 'Classes', href: '/modern.d20.srd/classes' },
+      { text: 'Combat', href: '/modern.d20.srd/combat' },
+      { text: 'Condition Summary', href: '/modern.d20.srd/conditions' },
+      { text: 'Creature Types', href: '/modern.d20.srd/creature.types' },
+      { text: 'Creatures', href: '/modern.d20.srd/creatures' },
+      { text: 'Environment & Hazards', href: '/modern.d20.srd/environment.hazards' },
+      { text: 'Equipment', href: '/modern.d20.srd/equipment' },
+      { text: 'Feats', href: '/modern.d20.srd/feats' },
+    ],
+  },
+  {
+    title: 'Future d20 SRD',
+    links: [
+      { text: 'Classes', href: '/future.d20.srd/classes' },
+      { text: 'Cybernetics', href: '/future.d20.srd/cybernetics' },
+      { text: 'Environments', href: '/future.d20.srd/environments' },
+      { text: 'Equipment', href: '/future.d20.srd/equipment' },
+      { text: 'Expanded Skills', href: '/future.d20.srd/expanded.skills' },
+      { text: 'Feats', href: '/future.d20.srd/feats' },
+      { text: 'Mecha', href: '/future.d20.srd/mecha' },
+      { text: 'Mutations', href: '/future.d20.srd/mutations' },
+      { text: 'Progress Levels', href: '/future.d20.srd/progress.levels' },
+    ],
+  },
+  {
+    title: 'Arcana d20 SRD',
+    links: [
+      { text: 'Advanced Classes', href: '/arcana.d20.srd/advanced.classes' },
+      { text: 'Creatures', href: '/arcana.d20.srd/creatures' },
+      { text: 'Equipment', href: '/arcana.d20.srd/equipment' },
+      { text: 'Feats', href: '/arcana.d20.srd/feats' },
+      { text: 'FX Items', href: '/arcana.d20.srd/fx.items' },
+      { text: 'Incantations', href: '/arcana.d20.srd/incantations' },
+      { text: 'Occupations', href: '/arcana.d20.srd/occupations' },
+      { text: 'Organizations', href: '/arcana.d20.srd/organizations' },
+      { text: 'Prestige Classes', href: '/arcana.d20.srd/prestige.classes' },
+    ],
+  },
+  {
+    title: 'd20 Menaces',
+    links: [
+      { text: 'Buying Services', href: '/menaces.d20/buying.services' },
+      { text: 'Feats', href: '/menaces.d20/feats' },
+      { text: 'Menaces', href: '/menaces.d20/menaces' },
+    ],
+  },
+],
 
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
